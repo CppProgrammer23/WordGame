4075824f5118116c0ce885ac48baad39e82da4f2
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2020-11-27 20:28:52 +0100

    Add files via upload

--- app/src/main/java/classes/Word.kt
+++ app/src/main/java/classes/Word.kt
@@ -2,18 +2,6 @@ package classes
 
 class Word (var mot:String, var taille:Int, var type:String, var genre:String) {
     override fun toString(): String {
-        return "Word(he word= '$mot', taille='$taille', type='$type',genre='$genre')"
-    }
-    fun getWord():String{
-        return mot
-    }
-    fun get_type():String{
-        return type
-    }
-    fun getSize():Int{
-        return taille
-    }
-    fun get_genre():String{
-        return genre
+        return "Word= '$mot', size='$taille', type='$type',genre='$genre'"
     }
 }
