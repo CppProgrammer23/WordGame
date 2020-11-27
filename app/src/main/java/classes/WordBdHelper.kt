53d9be0faa7625509271b0c78267f5af56347658
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2020-11-27 20:29:49 +0100

    Add files via upload

--- app/src/main/java/classes/WordBdHelper.kt
+++ app/src/main/java/classes/WordBdHelper.kt
@@ -6,18 +6,19 @@ import android.content.Context
 
 class WordBdHelper(context:Context?): SQLiteOpenHelper(context, "WordsGame.db",null,1) {
     companion object {
-        //val TABLE1 = "Tmot"
         val THE_TABLE = "Tmot"
+        val THE_SCORE="TScore"
         val COL_MOT = "mot"
         val COL_TAILLE = "taille"
         val COL_TYPE = "type"
         val COL_GENRE="genre"
+        val COL_SCORE="score"
     }
 
     override fun onCreate(db: SQLiteDatabase) {
         val CREATE_WORDS_TABLE =
             ("CREATE TABLE " + THE_TABLE + "(" + COL_MOT + " TEXT PRIMARY KEY, " + COL_TAILLE + " INTEGER NOT NULL, "
-                    + COL_TYPE + " TEXT NOT NULL, " + COL_GENRE+" TEXT NOT NULL"+");")
+                    + COL_TYPE + " TEXT NOT NULL, " + COL_GENRE+" TEXT NOT NULL "+");")
         db?.execSQL(CREATE_WORDS_TABLE)
     }
 
--- app/src/main/java/classes/WordManager.kt
+++ app/src/main/java/classes/WordManager.kt
@@ -30,9 +30,6 @@ class WordManager(context: Context) {
         values.put(WordBdHelper.COL_TYPE,mot.type)
         return wordHelp.writableDatabase.insert(WordBdHelper.THE_TABLE,null,values)
     }
-    //fun cursorToWords(cursor: Cursor):List<Word>{
-
-    //}
 
     fun getWords():List<Word>{
         val wordList: ArrayList<Word> = ArrayList()
@@ -60,22 +57,93 @@ class WordManager(context: Context) {
         return wordList
     }
 
-    //fun getWords(t:Int):List<Word>{
-
-    //}
-    //fun getWords(type:String, g:String):List<Word>{
-
-    //}
 
     fun deleteWord(m:String):Int{
-        return db.delete(WordBdHelper.THE_TABLE,WordBdHelper.COL_MOT+"=", arrayOf(m))//?
+        return db.delete(WordBdHelper.THE_TABLE,WordBdHelper.COL_MOT+" LIKE ?", arrayOf(m))//?
     }
     fun updateWord(m:String, w:Word):Int{
         val values=ContentValues()
         values.put(WordBdHelper.COL_MOT, w.mot)
         values.put(WordBdHelper.COL_TYPE,w.type)
         values.put(WordBdHelper.COL_GENRE,w.genre)
-        values.put(WordBdHelper.COL_TAILLE,m.length)
-        return db.update(WordBdHelper.THE_TABLE,values,"mot="+m,null)
+        values.put(WordBdHelper.COL_TAILLE,w.mot.length)
+        return db.update(WordBdHelper.THE_TABLE,values,"mot LIKE ?",arrayOf(m))
+    }
+    fun createTabScore(){
+        val createQuery =  "CREATE TABLE IF NOT EXISTS "+WordBdHelper.THE_SCORE+" (score INTEGER);"
+        try{
+            //cursor=db.rawQuery(createQuery,null)
+            db.execSQL(createQuery)
+        }
+        catch(e:SQLiteException){
+            //db.execSQL(createQuery)
+            e.printStackTrace()
+        }
+    }
+    fun insertScore(sc:Int):Long{
+        val values = ContentValues()
+
+        values.put("score",sc)
+        return wordHelp.writableDatabase.insert(WordBdHelper.THE_SCORE,null,values)
+    }
+    fun getScore():Int{
+        val selectQuery =  "SELECT score FROM "+ WordBdHelper.THE_SCORE+" ORDER BY score DESC LIMIT 1;"
+        var cursor :Cursor?=null
+        try{
+            cursor=db.rawQuery(selectQuery,null)
+        }
+        catch(e:SQLiteException){
+            db.execSQL(selectQuery)
+        }
+        var getsc: Int
+        cursor!!.moveToPosition(0)
+        getsc=cursor.getInt(cursor.getColumnIndex("score"))
+        return getsc
+    }
+    fun getRandomNoun():String{
+        val selectQuery =  "SELECT mot FROM "+WordBdHelper.THE_TABLE+" WHERE type LIKE ?"
+        var cursor :Cursor?=null
+
+        try{
+            cursor=db.rawQuery(selectQuery, arrayOf("Noun"))
+        }
+        catch(e:SQLiteException){
+            db.execSQL(selectQuery)
+        }
+        val sizeCursor = cursor!!.count
+        val rnd = (0 until sizeCursor-1).random()
+        var setMot=""
+        cursor.moveToPosition(rnd)
+        setMot=cursor.getString(cursor.getColumnIndex("mot"))
+        return setMot
+    }
+    fun getRandomAdj():String{
+        val selectQuery =  "SELECT mot FROM "+WordBdHelper.THE_TABLE+" WHERE type LIKE ?"
+        var cursor :Cursor?=null
+
+        try{
+            cursor=db.rawQuery(selectQuery, arrayOf("Adjective"))
+
+        }
+        catch(e:SQLiteException){
+            db.execSQL(selectQuery)
+
+        }
+        val sizeCursor = cursor!!.count
+        val rnd = (0 until sizeCursor-1).random()
+        var setMot=""
+        cursor.moveToPosition(rnd)
+        setMot=cursor.getString(cursor.getColumnIndex("mot"))
+        return setMot
+    }
+    fun dropTab():Unit{
+        val selectQuery =  "DROP TABLE IF EXISTS "+WordBdHelper.THE_SCORE+";"
+
+        try{
+            db.execSQL(selectQuery)
+        }
+        catch(e: SQLiteException){
+            e.printStackTrace()
+        }
     }
 }
